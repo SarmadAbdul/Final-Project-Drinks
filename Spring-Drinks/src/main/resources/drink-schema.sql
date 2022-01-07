514eebd1afa150475efaaa8cc335f2317aae9a54
sarmadabdul <sa.abdulkareem96@gmail.com>
2022-01-07 10:23:56 +0000

    resources completed

---
+++ Spring-Drinks/src/main/resources/drink-schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `drink` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `alcoholic_percentage` bigint DEFAULT NULL,
+  `calories` bigint DEFAULT NULL,
+  `carbohydrates` bigint DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
