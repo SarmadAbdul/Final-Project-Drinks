aa4e5b614eb6b8c37ac390f198872a777955e691
sarmadabdul <sa.abdulkareem96@gmail.com>
2022-01-09 17:25:11 +0000

    test is done

---
+++ Spring-Drinks/src/test/java/com/example/springdrinks/controller/DrinkControllerIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.example.springdrinks.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.springdrinks.domain.Drink;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:drink-schema.sql", "classpath:drink-data.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
+public class DrinkControllerIntegrationTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired 
+	private ObjectMapper mapper;
+	
+	@Test
+	public void createTest() throws Exception {
+		Drink entry = new Drink("Budweiser", "Lager", 4, 40000, 5);
+		Drink result = new Drink(2L, "Budweiser", "Lager", 4, 40000, 5);
+		
+		String entryAsJSON = this.mapper.writeValueAsString(entry);
+		String resultAsJSON = this.mapper.writeValueAsString(result);
+		
+		mvc.perform(post("/drink/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(entryAsJSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().json(resultAsJSON));
+	}
+	
+}
